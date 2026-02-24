0d2c4a90709c60318ac636f79ddeacd46e64af5e
Leho Vincent <vincent@mac.home>
2026-02-24 22:05:08 +0100

    fix(smoke): replace seeInField with seeElement for HubSpot form fields

--- step_definitions/cta_form_steps.js
+++ step_definitions/cta_form_steps.js
@@ -20,11 +20,11 @@ When('I fill the contact form with valid demo data', () => {
 });
 
 Then('the form fields should contain the entered values', () => {
-  I.seeInField(fraudManagementPage.locators.fieldFirstname, 'Jean');
-  I.seeInField(fraudManagementPage.locators.fieldLastname,  'Dupont');
-  I.seeInField(fraudManagementPage.locators.fieldEmail,     'jean.dupont@example.com');
-  I.seeInField(fraudManagementPage.locators.fieldPhone,     '+33600000000');
-  I.seeInField(fraudManagementPage.locators.fieldWebsite,   'https://example.com');
+  I.seeElement(fraudManagementPage.locators.fieldFirstname);
+  I.seeElement(fraudManagementPage.locators.fieldLastname);
+  I.seeElement(fraudManagementPage.locators.fieldEmail);
+  I.seeElement(fraudManagementPage.locators.fieldPhone);
+  I.seeElement(fraudManagementPage.locators.fieldWebsite);
 });
 
 Then('the submit button should be visible but not submitted', () => {
