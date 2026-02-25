d04312855821754b6de3dece558322d20d9d320f
Leho Vincent <vincent@mac.home>
2026-02-25 15:16:40 +0100

    test ci

--- step_definitions/cta_form_steps.js
+++ step_definitions/cta_form_steps.js
@@ -11,20 +11,25 @@ Then('the contact form should be loaded and ready', () => {
 
 When('I fill the contact form with valid demo data', () => {
   I.waitForElement(fraudManagementPage.locators.fieldFirstname, 10);
+  I.scrollTo(fraudManagementPage.locators.fieldFirstname);
+  I.waitForEnabled(fraudManagementPage.locators.fieldFirstname, 5);
   I.fillField(fraudManagementPage.locators.fieldFirstname, 'Jean');
   I.fillField(fraudManagementPage.locators.fieldLastname,  'Dupont');
   I.fillField(fraudManagementPage.locators.fieldEmail,     'jean.dupont@example.com');
   I.fillField(fraudManagementPage.locators.fieldPhone,     '+33600000000');
   I.fillField(fraudManagementPage.locators.fieldWebsite,   'https://example.com');
+  I.selectOption(fraudManagementPage.locators.fieldRevenue,  'Less than 500 000€');
+  I.selectOption(fraudManagementPage.locators.fieldBusiness, 'Online');
   I.fillField(fraudManagementPage.locators.fieldMessage,   'Je souhaite une démo de la solution anti-fraude HiPay.');
 });
 
 Then('the form fields should contain the entered values', () => {
-  I.seeElement(fraudManagementPage.locators.fieldFirstname);
-  I.seeElement(fraudManagementPage.locators.fieldLastname);
-  I.seeElement(fraudManagementPage.locators.fieldEmail);
-  I.seeElement(fraudManagementPage.locators.fieldPhone);
-  I.seeElement(fraudManagementPage.locators.fieldWebsite);
+  I.seeInField(fraudManagementPage.locators.fieldFirstname, 'Jean');
+  I.seeInField(fraudManagementPage.locators.fieldLastname,  'Dupont');
+  I.seeInField(fraudManagementPage.locators.fieldEmail,     'jean.dupont@example.com');
+  I.seeInField(fraudManagementPage.locators.fieldPhone,     '+33600000000');
+  I.seeInField(fraudManagementPage.locators.fieldWebsite,   'https://example.com');
+  I.seeInField(fraudManagementPage.locators.fieldMessage,   'Je souhaite une démo de la solution anti-fraude HiPay.');
 });
 
 Then('the submit button should be visible but not submitted', () => {
